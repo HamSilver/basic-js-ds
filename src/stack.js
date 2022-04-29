b515333f6f369e473eea420690085d6d9a7bdd6b
Stanislav Kolpakov <kolpakoff@gmail.com>
2022-04-29 23:12:26 +0200

    feat: Add stack solution

--- src/stack.js
+++ src/stack.js
@@ -13,20 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  constructor() { this.stack = [] }
+  push(e) { this.stack.push(e) }
+  pop() { return this.stack.pop() }
+  peek() { return [...this.stack].reverse()[0] }
 }
 
 module.exports = {
